1fd89532dc4aafb3a36c02bb3a4f4fedbd4613c8
SurajDhongadiShivashankar <suraj.dhongadi.s@gmail.com>
2023-04-26 23:36:12 +0530

    data_transformation.py | Utils implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,15 @@
 import os
 import sys
+import sys
+sys.path.append('E:\Projects\Git\DataScienceProject')
 from src.exception import CustomException
 from src.logger import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -42,7 +46,7 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
 
             logging.info("Train test split initiated")
-            train_data, test_data = train_test_split(self.ingestion_config.raw_data_path, train_size=0.2, random_state=42)
+            train_data, test_data = train_test_split(df, train_size=0.2, random_state=42)
 
             df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             train_data.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
@@ -62,4 +66,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    data_transformation.initiate_data_transformation(train_data, test_data)
